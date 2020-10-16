ed050da5bfef701a538ad83334cffb4f50e62c60
Shiven Tripathi <58358532+ShivenTripathi@users.noreply.github.com>
2020-10-16 22:56:46 +0530

    Create SymmetricTree.cpp
    A solution to Symmetric Tree on Leetcode

---
+++ problems/SymmetricTree.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isTrue(TreeNode* left,TreeNode* right){
+        if(!left&&!right)return true;
+        if(!left||!right)return false;
+     return left->val==right->val&&isTrue(left->right,right->left)&&isTrue(left->left,right->right);        
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(!root)return true;        
+        return isTrue(root->left,root->right);
+    }
+};
